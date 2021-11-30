66cf74b6e2039e4d7adc2612f40b5f9bf16803b9
Shivam Wadhawan <89898426+ShivWadDigi@users.noreply.github.com>
2021-11-30 11:58:25 +0530

    Major changes

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ function checkInput() {
     finalArray.splice(index, 0, coordObject);
   }
   var flag = "x";
-
+  var leftFlag = 1
+  var rightFlag = 1;
   for (let i = 1; i < finalArray.length; i++) {
     //Taking Previous component and next component
     let prevComponent = Object.keys(finalArray[i - 1])[0];
@@ -38,44 +39,93 @@ function checkInput() {
           finalArray[i - 1][prevComponent].coord['x'] + 0.8;
         finalArray[i][nextComponent].rotation.y =
           finalArray[i - 1][prevComponent].rotation.y;
-        finalArray[i][nextComponent].coord['z'] = finalArray[i][prevComponent].coord['z'];
+        finalArray[i][nextComponent].coord['z'] = finalArray[i - 1][prevComponent].coord['z'];
       }
       else {
         flag = 'z'
         finalArray[i][nextComponent].coord['z'] = finalArray[i - 1][prevComponent].coord['z'] + 0.8;
         finalArray[i][nextComponent].rotation.y = finalArray[i - 1][prevComponent].rotation.y;
-        finalArray[i][nextComponent].coord['x'] = finalArray[i][prevComponent].coord['x'];
+        finalArray[i][nextComponent].coord['x'] = finalArray[i - 1][prevComponent].coord['x'];
 
       }
 
     } else if (prevComponent == "right") {
-      if (flag == "x") {
-        flag = "z";
-        finalArray[i][nextComponent].coord['x'] =
-          finalArray[i - 1][prevComponent].coord['x'];
-        finalArray[i][nextComponent].rotation.y =
-          finalArray[i - 1][prevComponent].rotation.y - 90;
-      } else flag = "x";
-      finalArray[i][nextComponent].coord['z'] =
-        finalArray[i - 1][prevComponent].coord['z'];
-      finalArray[i][nextComponent].rotation.y =
-        finalArray[i - 1][prevComponent].rotation.y - 90;
+      if (rightFlag < 3) {
+        rightFlag += 1;
+        if (flag == "x") {
+          flag = "z";
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'];
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y - 90;
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'] + 0.8;
+        } else {
+          flag = "x";
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'];
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y - 90;
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'] + 0.8;
+        }
+      }
+      else if (rightFlag > 2) {
+        if (flag == "x") {
+          flag = "z";
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'];
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y - 90;
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'] - 0.8;
+        } else {
+          flag = "x";
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'];
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y - 90;
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'] - 0.8;
+        }
+      }
     } else if (prevComponent == "left") {
-      if (flag == "x") {
-        flag = "z";
-        finalArray[i][nextComponent].coord['x'] =
-          finalArray[i - 1][prevComponent].coord['x'];
-        finalArray[i][nextComponent].coord['z'] = finalArray[i - 1][prevComponent].coord['z'] + 0.8;
-        finalArray[i][nextComponent].rotation.y =
-          finalArray[i - 1][prevComponent].rotation.y + 90;
+      if (leftFlag < 3) {
+        leftFlag += 1;
+        if (flag == "x") {
+          flag = "z";
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'];
+          finalArray[i][nextComponent].coord['z'] = finalArray[i - 1][prevComponent].coord['z'] + 0.8;
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y + 90;
 
-      } else {
-        flag = "x";
-        finalArray[i][nextComponent].coord['z'] =
-          finalArray[i - 1][prevComponent].coord['z'];
-        finalArray[i][nextComponent].coord['x'] = finalArray[i - 1][prevComponent].coord['x'] + 0.8;
-        finalArray[i][nextComponent].rotation.y =
-          finalArray[i - 1][prevComponent].rotation.y + 90;
+        } else {
+          flag = "x";
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'];
+          finalArray[i][nextComponent].coord['x'] = finalArray[i - 1][prevComponent].coord['x'] + 0.8;
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y + 90;
+        }
+      }
+      else if (leftFlag > 2) {
+        if (flag == "x") {
+          flag = "z";
+          finalArray[i][nextComponent].coord['x'] =
+            finalArray[i - 1][prevComponent].coord['x'];
+          finalArray[i][nextComponent].coord['z'] = finalArray[i - 1][prevComponent].coord['z'] - 0.8;
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y + 90;
+
+        } else {
+          flag = "x";
+          finalArray[i][nextComponent].coord['z'] =
+            finalArray[i - 1][prevComponent].coord['z'];
+          finalArray[i][nextComponent].coord['x'] = finalArray[i - 1][prevComponent].coord['x'] - 0.8;
+          finalArray[i][nextComponent].rotation.y =
+            finalArray[i - 1][prevComponent].rotation.y + 90;
+        }
       }
 
     }
